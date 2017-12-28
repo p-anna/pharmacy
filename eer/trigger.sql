c5458fb10488551a23345d0f17489089b5fc6b34
MrHofmann <mdfkprekid@gmail.com>
2017-12-28 01:07:53 +0100

    Prepravke... 28.12.2017.

--- eer/trigger.sql
+++ eer/trigger.sql
@@ -7,14 +7,28 @@ DROP TRIGGER IF EXISTS PRODAJA$
 CREATE TRIGGER PRODAJA
 AFTER INSERT ON KUPOVNIA
 FOR EACH ROW
+--DEFINISANJE POMOCNE PROMENLJIVE KOJA JE INICJIJALNO JEDNAKA NEW.KOLICINA
 BEGIN
-	IF( new.kolicina > ( SELECT sum(M0.kolicina) FROM MAGACIN M0 WHERE M0.id_proizvoda = new.id_proizvoda) THEN 
-		SIGNAL sqlstate='45000' SET message_text='NEMA DOVOLJNO TRAZENOG PROIZVODA U MAGACINU';
+	IF (new.kolicina > ( 	SELECT sum(M0.kolicina) 
+				FROM MAGACIN M0 
+				WHERE M0.id_proizvoda = new.id_proizvoda
+					AND M0.rok_trajanja > current_date
+			) THEN 
+		SIGNAL sqlstate='45000' SET message_text='NEMA DOVOLJNO TRAZENOG PROIZVODA';
 	ELSE 
-		UPDATE MAGACIN M1 SET M1.kolicina = new.kolicina 
-		WHERE M1.id_proizvoda = new.id_proizvoda AND M1.rok_trajanja IN(SELECT min(M2.rok_trajanja)
-										FROM MAGACIN M2
-										WHERE M2.id_proizvoda = M1.id_proizvoda
-										GROUP BY M2.id_proizvoda);
+--FOR PETLJA U KOJOJ SE KOLICINE PROIZVODA SMANJUJU DOK SE NE DOSTIGNE VREDNOST POMOCNE PROMENLJIVE
+--		UPDATE MAGACIN M1 SET M1.kolicina = M1.kolicina-new.kolicina 
+--		WHERE M1.id_proizvoda = new.id_proizvoda AND M1.rok_trajanja 
+	END IF;
+END$
+
+
+DROP TRIGGER IF EXISTS UPDATE_MAGACIN$
+
+CREATE TRIGGER UPDATE_MAGACIN
+AFTER UPDATE ON MAGACIN
+FOR EACH ROW
+BEGIN
+	IF(new.kolicina = 0) THEN DELETE
 	END IF;
 END$
