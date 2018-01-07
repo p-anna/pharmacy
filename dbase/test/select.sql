aad793b2658203c766d23175875aa86dca54f1ca
MrHofmann <mdfkprekid@gmail.com>
2018-01-08 00:09:44 +0100

    Select izmena.

--- dbase/test/select.sql
+++ dbase/test/select.sql
@@ -7,6 +7,8 @@ select id_proizvoda, sum(kolicina)
 from MAGACIN
 group by id_proizvoda;
 
-select R.id_racuna, id_korisnika, datum_izdavanja, iznos, id_proizvoda, kolicina
+select R.id_racuna, id_proizvoda, kolicina, iznos, datum_izdavanja, id_korisnika
 from RACUN R join KUPOVINA K on K.id_racuna = R.id_racuna
 order by datum_izdavanja;
+
+select * from RACUN;
